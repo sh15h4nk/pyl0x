efd9fc6c1aeb003f3873ef77cb330a0037801dfe
sh15h4nk <shiva.shvvs@gmail.com>
2022-02-16 00:15:37 +0530

    updated error handler

--- src/pylox/error_reporter.py
+++ src/pylox/error_reporter.py
@@ -1,6 +1,9 @@
 """Error Reporters"""
+from pylox.scanner.token_types import TOKEN_TYPES
+
 def report(line, where, message):
     print("Error:", "[line: "+str(line)+"]", where, ":", message)
 
-def error(line, message):
-    return report(line, "", message)
+def error(token, message):
+    if token.type == TOKEN_TYPES.EOF: return report(token.line, "at end", message)
+    return report(token.line, "at '" + str(token.lexeme) + "' ", message)
