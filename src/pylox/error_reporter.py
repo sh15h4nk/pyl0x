f05ebe82c4620389840927b3081421da5c617661
sh15h4nk <shiva.shvvs@gmail.com>
2022-02-18 22:31:23 +0530

    added run time error handler

--- src/pylox/error_reporter.py
+++ src/pylox/error_reporter.py
@@ -7,3 +7,6 @@ def report(line, where, message):
 def error(token, message):
     if token.type == TOKEN_TYPES.EOF: return report(token.line, "at end", message)
     return report(token.line, "at '" + str(token.lexeme) + "' ", message)
+
+def run_time_error(error):
+    print("{} \n[line {} ]".format(error.getMessage(), error.token.line))
