385f772c827f911d1a40ee4d5f5badbf83a90ab1
sh15h4nk <shiva.shvvs@gmail.com>
2022-06-27 00:58:50 +0530

    fixed env issue

--- src/pylox/environment/environment.py
+++ src/pylox/environment/environment.py
@@ -1,13 +1,15 @@
 
 from pylox.exceptions.runtime_error import runtime_error
 
-
 class Environment:
-    values = dict()
     enclosing = None
     
-    def __init__(self, enclose = None):
+    def __init__(self, enclose = None) -> None:
         self.enclosing = enclose
+        self.values = dict()
+    
+    def __repr__(self) -> str:
+        return str(self.values)
         
     
     def get(self, name):
@@ -16,7 +18,7 @@ class Environment:
         if self.enclosing is not None: return self.enclosing.get(name)
         raise runtime_error(name, "Undefined variable '" + name.lexeme + "'.")
     
-    def assign(self, name, value):
+    def assign(self, name, value) -> None:
         if name.lexeme in self.values:
             self.values[name.lexeme] = value
             return
